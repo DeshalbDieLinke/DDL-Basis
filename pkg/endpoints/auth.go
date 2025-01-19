977f68c61d559024138c7444d5f9b03913ba63bc
Julian <81575762+Brownymister@users.noreply.github.com>
2025-01-19 17:35:02 +0100

    init endpoints

---
+++ pkg/endpoints/auth.go
@@ -0,0 +1,18 @@
+package endpoints
+
+import (
+    "github.com/labstack/echo/v4"
+)
+
+func Login(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello, World!")
+}
+
+func Logout(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello, World!")
+}
+
+func CheckAuth(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello, World!")
+}
+
--- pkg/endpoints/endpoints.go
+++ pkg/endpoints/endpoints.go
@@ -8,3 +8,16 @@ import (
 func HelloWorld(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
+
+
+func GetContent(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello, World!")
+}
+
+func SearchContent(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello, World!")
+}
+
+func CreateContent(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello, World!")
+}
