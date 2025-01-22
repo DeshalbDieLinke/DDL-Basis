e3fcb27d4dbd88081431365aefeccf53b9784ff2
demohstens <demohstens@gmail.com>
2025-01-22 11:12:30 +0100

    added domain and cache of cert

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"deshalbdielinke/pkg/endpoints"
 
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	// db := database.StartDatabase()
 
 	echo := echo.New()
+
+	// Cache the certificate to prevent rate limiting
+	echo.AutoTLSManager.Cache = autocert.DirCache("~/certs")
+	echo.AutoTLSManager.HostPolicy = autocert.HostWhitelist("api.deshalbdielinke.de")
+
 	echo.GET("/", endpoints.HelloWorld)
 	echo.GET("/content", endpoints.GetContent)
 	echo.Logger.Fatal(echo.StartAutoTLS(":443"))
