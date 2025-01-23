d7349ada4f32b5b0ff1f50720aa8ec42c40a474d
Demohstens <demohstens@gmail.com>
2025-01-24 00:39:52 +0100

    updated CORS

--- prod.go
+++ prod.go
@@ -39,7 +39,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
+		AllowOrigins: []string{"https://deshalbdielinke.de", "https://api.deshalbdielinke.de"},
 		AllowMethods: []string{"GET", "POST"},
 	}))
 	e.Use(jwtE.WithConfig(jwtE.Config{
