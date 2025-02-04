ab9149c7efa90c8164caf9c886005a97cce23186
Demohstens <demohstens@gmail.com>
2025-02-04 13:48:41 +0100

    s

--- pkg/endpoints/clerk.go
+++ pkg/endpoints/clerk.go
@@ -1,29 +1,20 @@
 package endpoints
 
 import (
-	"net/http"
+	"log"
 
-	"github.com/clerk/clerk-sdk-go/v2"
-	"github.com/clerk/clerk-sdk-go/v2/user"
 	"github.com/labstack/echo/v4"
 )
 
 func UserCreated(c echo.Context) error {
 		ctx := c.Request().Context()
 	
-		claims, ok := clerk.SessionClaimsFromContext(ctx)
-		if !ok {
-			return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized"})
-		}
+		log.Fatalf("User created" + ctx.Value("type").(string))
+		// if usr == nil {
+		// 	return c.JSON(http.StatusNotFound, map[string]string{"message": "User does not exist"})
+		// }
 
-		usr, err := user.Get(ctx, claims.Subject)
-		if err != nil {
-			panic(err)
-		}
-		if usr == nil {
-			return c.JSON(http.StatusNotFound, map[string]string{"message": "User does not exist"})
-		}
-
-		return c.JSON(http.StatusOK, map[string]string{"message": "User created" + usr.ID})
+		// return c.JSON(http.StatusOK, map[string]string{"message": "User created" + usr.ID})
+		return c.JSON(200, map[string]string{"message": "User created"})
 	}
 
