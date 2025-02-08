1799f4f6cb8476fa9ff0d5db0bc6bea5228a58a7
Demohstens <Demohstens@gmail.com>
2025-02-08 23:26:36 +0100

    only content endpoint

--- main.go
+++ main.go
@@ -31,20 +31,19 @@ func main() {
 	var CLERK_KEY = os.Getenv("CLERK_KEY")
 
 	var ALLOWED_ORIGINS = "http://192.168.0.194:3000"
-	clerk.SetKey(CLERK_KEY)
 
 	// Register middleware
 	e.Use(database.GormMiddleware(db))
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		// NO TRAILING / ALLOWED
-		AllowOrigins:     []string{ALLOWED_ORIGINS},
-		AllowMethods:     []string{echo.GET, echo.POST, echo.OPTIONS},
-		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, "credentials"},
-		AllowCredentials: true,
-	}))
-	log.Printf("Allowed origins: %v", []string{ALLOWED_ORIGINS})
+	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	// 	// NO TRAILING / ALLOWED
+	// 	AllowOrigins:     []string{"*"},
+	// 	AllowMethods:     []string{echo.GET, echo.POST, echo.OPTIONS},
+	// 	AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, "credentials"},
+	// 	AllowCredentials: true,
+	// }))
+	// log.Printf("Allowed origins: %v", []string{ALLOWED_ORIGINS})
 	// e.Use(jwtE.WithConfig(jwtE.Config{
 	// 	Skipper: func(c echo.Context) bool {
 	// 		return !strings.Contains(c.Path(), "/auth")
@@ -70,53 +69,53 @@ func main() {
 	// e.POST("/register", endpoints.Register)
 	// e.POST("/login", endpoints.LoginJwt)
 	e.GET("/", endpoints.HelloWorld)
-	e.GET("/topics", endpoints.Topics)
+	// e.GET("/topics", endpoints.Topics)
 	e.GET("/content", endpoints.GetContent)
-	e.GET("/profile", endpoints.Profile)
-	// e.GET("/logout", endpoints.Logout)
-	// e.GET("/upload", endpoints.CreateContentNew)
-	// Register a catch-all route
-	e.Any("/*", func(context echo.Context) error {
-		return context.String(404, "Not Found")
-	})
+	// e.GET("/profile", endpoints.Profile)
+	// // e.GET("/logout", endpoints.Logout)
+	// // e.GET("/upload", endpoints.CreateContentNew)
+	// // Register a catch-all route
+	// e.Any("/*", func(context echo.Context) error {
+	// 	return context.String(404, "Not Found")
+	// })
 
-	// Register Restricted Endpoints
-	restricted := e.Group("/auth")
-	// restricted.GET("/profile", endpoints.Profile)
-	restricted.POST("/upload", endpoints.CreateContent)
-	restricted.POST("/update-content", endpoints.UpdateContent)
-	restricted.POST("/delete-content", endpoints.DeleteContentItem)
+	// // Register Restricted Endpoints
+	// restricted := e.Group("/auth")
+	// // restricted.GET("/profile", endpoints.Profile)
+	// restricted.POST("/upload", endpoints.CreateContent)
+	// restricted.POST("/update-content", endpoints.UpdateContent)
+	// restricted.POST("/delete-content", endpoints.DeleteContentItem)
 
-	restricted.GET("/users", endpoints.GetUsers)
-	// restricted.POST("/update-user", endpoints.UpdateUser)
-	// restricted.GET("/check", endpoints.Check)
-	// restricted.POST("/new-user", endpoints.NewUserToken)
-	restricted.GET("/*", func(c echo.Context) error {
-		log.Printf("Authenticated request")
-		return c.JSON(200, map[string]string{"message": "Authenticated request"})
-	})
+	// restricted.GET("/users", endpoints.GetUsers)
+	// // restricted.POST("/update-user", endpoints.UpdateUser)
+	// // restricted.GET("/check", endpoints.Check)
+	// // restricted.POST("/new-user", endpoints.NewUserToken)
+	// restricted.GET("/*", func(c echo.Context) error {
+	// 	log.Printf("Authenticated request")
+	// 	return c.JSON(200, map[string]string{"message": "Authenticated request"})
+	// })
 
-	// e.Use(endpoints.ClerkMiddleware)
+	// // e.Use(endpoints.ClerkMiddleware)
 
-	// Check if DB is empty and create a default user
-	// var count int64
-	// db.Model(&models.User{}).Count(&count)
-	// if count == 0 {
-	// 	adminEmail := os.Getenv("INIT_EMAIL")
-	// 	if adminEmail == "" {
-	// 		log.Fatal("No admin email provided")
-	// 	}
-	// 	initToken, _ := utils.GenerateToken(adminEmail, 0)
-	// 	log.Printf("Creating default user for %v: %s", adminEmail, initToken)
-	// } else {
-	// 	log.Printf("Count: %d", count)
-	// }
+	// // Check if DB is empty and create a default user
+	// // var count int64
+	// // db.Model(&models.User{}).Count(&count)
+	// // if count == 0 {
+	// // 	adminEmail := os.Getenv("INIT_EMAIL")
+	// // 	if adminEmail == "" {
+	// // 		log.Fatal("No admin email provided")
+	// // 	}
+	// // 	initToken, _ := utils.GenerateToken(adminEmail, 0)
+	// // 	log.Printf("Creating default user for %v: %s", adminEmail, initToken)
+	// // } else {
+	// // 	log.Printf("Count: %d", count)
+	// // }
 
-	delta1 := time.Now()
-	// utils.SyncFileContent(db)
-	delta2 := time.Now()
-	log.Printf("Syncing content took: %v", delta2.Sub(delta1))
-	// Start the server
+	// delta1 := time.Now()
+	// // utils.SyncFileContent(db)
+	// delta2 := time.Now()
+	// log.Printf("Syncing content took: %v", delta2.Sub(delta1))
+	// // Start the server
 	e.Logger.Fatal(e.Start("127.0.0.1:8080"))
 
 }
